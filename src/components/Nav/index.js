3a0159f949dd04bfd2ceb8a092ccefcf33b66d91
Gary <52983305+Undisputed06@users.noreply.github.com>
2022-07-30 18:07:57 -0500

    form build

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import React, { useEffect } from 'react';
 
 function Nav(props) {
   const {
@@ -9,10 +8,9 @@ function Nav(props) {
     currentCategory,
   } = props;
 
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
-
+  useEffect(() =>{
+    document.title = capitalizeFirstLetter(currentCategory.name)
+  }, [currentCategory]) 
 
   return (
     <header className="flex-row px-1">
